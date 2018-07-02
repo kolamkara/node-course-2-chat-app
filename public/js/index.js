19dfdd4df89ef35fa2afc2cfe609628554efebdd
kolamkara <kiranjk97@gmail.com>
2018-07-02 13:13:02 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket=io();
 
 socket.on('connect',function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    from:'Andrew',
-    text:'Yup that works for me.'
-  });
 });
 
 socket.on('disconnect',function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage',function (email) {
-  console.log('newMessage',email);
+socket.on('newMessage',function (message) {
+  console.log('newMessage',message);
 });
--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from: 'John',
-    text: 'See you then',
-    createdAt:12335
-  });
-
-  socket.on('createMessage',(newMessage)=>{
-    console.log('createMessage',newMessage);
+  socket.on('createMessage',(message)=>{
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createdAt:new Date().getTime()
+    });
   });
 
   socket.on('disconnect',()=>{
